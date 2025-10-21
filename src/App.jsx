5ca4c50135f1f157f0ef9fedde21484539143bfc
Alejandro <alexmateosxd@gmail.com>
2025-10-21 10:56:34 +0200

    Primera implementacion de la pagina home del marketplace

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+
+import HomePage from './pages/Home/layout/HomeLayout'
+
+
+function App() {
+
+
+  return (
+    <>
+    
+    <HomePage  />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home/components/productosExpertos.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+function ProductosExpertos() {
+  const items = [
+    {
+      title: "Laptop Pro 16",
+      desc: "Powerful performance",
+      image: "../public/unnamed(2).png",
+    },
+    {
+      title: "Wireless Headphones X50",
+      desc: "Immersive sound",
+      image: "../public/unnamed(3).png",
+    },
+    {
+      title: "Smartwatch Series 7",
+      desc: "Advanced tracking",
+      image: "../public/unnamed(4).png",
+    },
+    {
+      title: "Portable Speaker SoundWave",
+      desc: "Compact power",
+      image: "../public/unnamed(5).png",
+    },
+  ];
+  return (
+    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div className="grid grid-cols-1  px-4 sm:px-0 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {items.map((item) => (
+          <div
+            key={item.title}
+            className="group overflow-hidden rounded-lg bg-surface-light shadow-sm transition-shadow hover:shadow-md dark:bg-surface-dark"
+          >
+            <img
+              alt={item.title}
+              className="h-48 w-full object-cover transition-transform duration-300 group-hover:scale-105"
+              src={item.image}
+            />
+            <div className="p-4">
+              <h3 className="font-bold">{item.title}</h3>
+              <p className="mt-1 text-sm text-subtle-light dark:text-subtle-dark">
+                {item.desc}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+export default ProductosExpertos;
