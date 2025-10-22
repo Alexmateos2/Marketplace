0f653a6b2dc49fa37c3509a17166278bea01785b
Alejandro <practicas2@sicosl.onmicrosoft.com>
2025-10-22 09:46:07 +0200

    Implementacion layout de producto, muestra producto estatico actualmente,implementacion de fuente Space Grotesk para mas elegancia, arreglado estilos varios

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/Home/layout/HomeLayout";
 import ProductsPage from "./pages/Products/layout/ProductsLayout";
 import AboutUsPage from "./pages/About us/layout/AboutUsLayout";
+import ProductPage from "./pages/Home/layout/ProductLayout";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/products" element={<ProductsPage />} />
         <Route path="*" element={<div>404 Not Found</div>} />
         <Route path="/about" element={<AboutUsPage />} />
+        <Route path="/product" element={<ProductPage />} />
       </Routes>
     </Router>
   );
