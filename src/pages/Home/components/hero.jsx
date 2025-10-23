5530cd01d41e5f5f82500ac0df3d02312564bea4
alexmateos <alexmateosxd@gmail.com>
2025-10-23 21:21:23 +0200

    Mejorado home hero en moviles

--- src/pages/Home/components/hero.jsx
+++ src/pages/Home/components/hero.jsx
@@ -7,19 +7,22 @@ const Hero = () => {
     <section className="relative h-[500px] overflow-hidden rounded-xl  lg:my-0">
       <img
         alt="Desk setup"
-        className="absolute inset-0 h-full w-full object-cover brightness-65 md:brightness-100"
+        className="absolute inset-0 h-full w-full object-cover brightness-100"
         loading="lazy"
         src="../unnamed(1).png"
       />
-      <div className="absolute inset-0 bg-gradient-to-r from-background-light via-background-light/70 to-transparent dark:from-background-dark dark:via-background-dark/50"></div>
+      <div className="absolute inset-0 bg-linear-to-r 
+  from-background-light/90 via-background-light/80 to-transparent
+  dark:from-background-dark dark:via-background-dark/50"></div>
       <div className="relative grid place-items-center h-full p-10 sm:p-25 lg:w-2/3 mx-auto text-center">
         <h1 className="text-5xl md:text-7xl font-black tracking-tighter mb-4">
           Tech, Perfected.
         </h1>
-        <p className="max-w-md text-base text-xl xs:text-lg text-subtle-light dark:text-gray-200">
-          Tekia ofrece una selección curada de tecnología de primer nivel,
-          revisada por expertos del sector.
-        </p>
+       <p className="max-w-md mb-2 text-xl xs:text-lg text-content-light dark:text-content-dark drop-shadow-md ">
+  Tekia ofrece una selección curada de tecnología de primer nivel,
+  revisada por expertos del sector.
+</p>
+
         <Link to="/products">
           <GlareButton>Explore Collections</GlareButton>
         </Link>
