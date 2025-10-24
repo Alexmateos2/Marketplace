29787b19239ee875f2314888b4385e636b3eb707
Alejandro <alexmateosxd@gmail.com>
2025-10-24 09:05:52 +0200

    Mejoras size distintas imagenes, implementado producto dinamico y aplicado en productos, mejora de links de imagenes de prueba

--- src/pages/Home/components/productosExpertos.jsx
+++ src/pages/Home/components/productosExpertos.jsx
@@ -1,44 +1,26 @@
 import React from "react";
+import ProductsItemList from "../../../shared/utils/ProductsItemList.jsx";
 function ProductosExpertos() {
-  const items = [
-    {
-      title: "Laptop Pro 16",
-      desc: "Powerful performance",
-      image: "../unnamed(2).png",
-    },
-    {
-      title: "Wireless Headphones X50",
-      desc: "Immersive sound",
-      image: "../unnamed(3).png",
-    },
-    {
-      title: "Smartwatch Series 7",
-      desc: "Advanced tracking",
-      image: "../unnamed(4).png",
-    },
-    {
-      title: "Portable Speaker SoundWave",
-      desc: "Compact power",
-      image: "../unnamed(5).png",
-    },
-  ];
+  const products = ProductsItemList
+  const items = products.slice(products.length - 4, products.length)
+
   return (
     <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <div className="grid grid-cols-1  px-8 sm:px-0 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {items.map((item) => (
           <div
-            key={item.title}
+            key={item.name}
             className="group overflow-hidden rounded-lg bg-surface-light shadow-sm transition-shadow hover:shadow-md dark:bg-surface-dark"
           >
             <img
-              alt={item.title}
+              alt={item.name}
               className="h-52 w-full object-cover transition-transform duration-300 group-hover:scale-105 "
               src={item.image}
             />
             <div className="px-4 py-6">
-              <h3 className="font-bold">{item.title}</h3>
+              <h3 className="font-bold">{item.name}</h3>
               <p className="mt-1 text-sm text-subtle-light dark:text-subtle-dark">
-                {item.desc}
+                {item.shortDesc}
               </p>
             </div>
           </div>
--- src/pages/Product/layout/ProductLayout.jsx
+++ src/pages/Product/layout/ProductLayout.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import Navbar from "../../../shared/navbar/navbar";
 import Footer from "../../../shared/Footer";
-
+import ProductsItemList from "../../../shared/utils/ProductsItemList.jsx";
+const products = ProductsItemList
+import { NavLink, useParams } from "react-router-dom";
 const ProductPage = () => {
+  const { id } = useParams();
+  // Si tu id es numérico, convierte: parseInt(id)
+  const product = products.find(p => String(p.id) === id);
+
+  if (!product) return <div>Producto no encontrado</div>;
+
+
   return (
     <div className="flex flex-col min-h-screen dark:bg-background-dark dark:text-content-dark bg-background-light text-content-light font-display transition-colors">
       <Navbar />
@@ -10,122 +19,110 @@ const ProductPage = () => {
         <div className="mx-auto max-w-5xl">
           <div className="mb-6">
             <nav className="flex items-center text-sm font-medium text-gray-500 dark:text-gray-400">
-              <a className="hover:text-primary" href="#">
+              <NavLink
+                to='/products'
+               className="hover:text-primary" href="#">
                 Tech
-              </a>
+              </NavLink>
               <span className="mx-2">/</span>
-              <span className="text-gray-700 dark:text-gray-300">Audio</span>
+              <NavLink to={`/products/${product.category}`}
+              className="text-gray-700 dark:text-gray-300 hover:text-primary">{product.category}</NavLink>
             </nav>
           </div>
 
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-12">
-            {/*Imágenes y especificaciones */}
-            <div className="flex flex-col order-2 lg:order-1">
-              <div className="grid grid-cols-2 grid-rows-2 gap-4 aspect-[4/3]">
-                <div className="col-span-2 row-span-2 rounded-xl overflow-hidden">
-                  <div
-                    className="w-full h-full bg-center bg-no-repeat bg-cover"
-                    style={{
-                      backgroundImage:
-                        'url("https://lh3.googleusercontent.com/aida-public/AB6AXuBX492x_T4neNW7Bxet6V4UUV9F6Z0qLQrvltFXCTIuYPe5DbATXJDXU-Kfhy5lEUH7Gqq82gX46JDHd9KH-d1lbBdqNmjCDUEjRYYTPJcBXoWeNu1hh1oS1Q_R0MSsn-RutOlF-R_X0G4ayLwoJSFbun43M8iTFbrh0m-VbC7GEUrmVt_p51_R04fdP_chDrhoxbW0rcC-I7goUkKRvXHPBZL9YOupBQKK-fN9ctqxWwPPX8oy9BmuUc5Bm24cnjBAKqSGrvXXWss")',
-                    }}
-                  ></div>
-                </div>
-                <div className="rounded-xl overflow-hidden">
-                  <div
-                    className="w-full h-full bg-center bg-no-repeat bg-cover"
-                    style={{
-                      backgroundImage:
-                        'url("https://lh3.googleusercontent.com/aida-public/AB6AXuA6fA2jPYZfQ5Zf_xSevCByaniM6tu2xMYSG7cb--7OuwKlwxSPQVsI57zGvj_tL6kg8eZau_QRx69SA3aMkp-Xf8tGe0ltLtl626FLGN0WHbvRYTH_UL90WDx5vn4TstqBfI0VqYVZYeU1nFNOue94Mqw8ci7HRySURHa-eeBfI0JyfckGmrgIjpt0jv9Y9LQSm3uUg5eiecWoWWkJW1oupWUG7qnZ5hsamBn0ZHG8uCCs3e0u45ZWYfwOvAPUCEdWanF6sE1YYlc")',
-                    }}
-                  ></div>
-                </div>
-                <div className="rounded-xl overflow-hidden">
-                  <div
-                    className="w-full h-full bg-center bg-no-repeat bg-cover"
-                    style={{
-                      backgroundImage:
-                        'url("https://lh3.googleusercontent.com/aida-public/AB6AXuAYlmmja244Fkd6a9HwotZXcbYXrRINJ0YqL6UxCh04IDyQwtf2ZpWddM-xqwPq5j2870EX4eBGymYoWkMI-uRQH1LT_Em3ItPyNLZbhqDbML1jU3rPE9fomea1mTp4O6erLJXntpWVpd3Qy6-sKOu2njHqiLpStxTZFKTTMS204VoHEGL_UbZ9V4PWw7AxJodVWmNZJggJ3zu9CQqhejnmvvZ-QBPgLD78CIvY-Bi1k1-vK2Bc2c-aSyAdKJfO9v4e-IIKnaWSLFA")',
-                    }}
-                  ></div>
-                </div>
-              </div>
 
-              <div className="mt-8">
-                <h2 className="text-2xl font-bold text-gray-900 dark:text-white mb-4">
-                  Specifications
-                </h2>
-                <div className="divide-y divide-gray-200 dark:divide-gray-800 border-t border-b border-gray-200 dark:border-gray-800">
-                  <div className="py-4 grid grid-cols-3 gap-4">
-                    <p className="text-sm font-medium text-gray-500 dark:text-gray-400">
-                      Connectivity
-                    </p>
-                    <p className="text-sm col-span-2 text-gray-800 dark:text-gray-200">
-                      Bluetooth 5.0
-                    </p>
-                  </div>
-                  <div className="py-4 grid grid-cols-3 gap-4">
-                    <p className="text-sm font-medium text-gray-500 dark:text-gray-400">
-                      Battery Life
-                    </p>
-                    <p className="text-sm col-span-2 text-gray-800 dark:text-gray-200">
-                      Up to 30 hours
-                    </p>
-                  </div>
-                  <div className="py-4 grid grid-cols-3 gap-4">
-                    <p className="text-sm font-medium text-gray-500 dark:text-gray-400">
-                      Noise Cancellation
-                    </p>
-                    <p className="text-sm col-span-2 text-gray-800 dark:text-gray-200">
-                      Active Noise Cancellation
-                    </p>
-                  </div>
-                  <div className="py-4 grid grid-cols-3 gap-4">
-                    <p className="text-sm font-medium text-gray-500 dark:text-gray-400">
-                      Weight
-                    </p>
-                    <p className="text-sm col-span-2 text-gray-800 dark:text-gray-200">
-                      250g
-                    </p>
-                  </div>
-                  <div className="py-4 grid grid-cols-3 gap-4">
-                    <p className="text-sm font-medium text-gray-500 dark:text-gray-400">
-                      Color
-                    </p>
-                    <p className="text-sm col-span-2 text-gray-800 dark:text-gray-200">
-                      Midnight Black
-                    </p>
-                  </div>
+          <div className="grid grid-cols-1 lg:grid-cols-2  gap-12">
+            {/* Producto */}
+            <div className="flex flex-col order-1 lg:order-2 ">
+              <div className=" rounded-xl px-6 mb-8 lg:mb-0 ">
+                <h1 className="text-3xl font-bold text-gray-900 dark:text-white mb-4 ">
+                 {product.name}
+                </h1>
+                <p className="text-gray-600 dark:text-gray-300 mb-6">
+                  {product.desc}
+                </p>
+                <div className="flex items-baseline gap-2 mb-6 ">
+                  <span
+                    className="text-4xl font-black text-gray-900 dark:text-white md:ml-0 ml-3"
+                    content="299"
+                    itemProp="price"
+                  >
+                   ${product.price}
+                  </span>
+                </div>
+                <button className="sm:w-full md:w-2/3 lg:w-full flex items-center justify-center rounded-lg h-12 px-6 bg-primary text-white text-base font-bold hover:bg-primary/90 transition-all">
+                  Add to Cart
+                </button>
+              </div>
+            </div>
+
+            {/* Imagenes */}
+            <div className="flex flex-col max-h-[350px] order-2 lg:order-1">
+              <div className="w-full max-w-[600px] mx-auto relative overflow-hidden rounded-xl aspect-[4/3]">
+                <img
+                  src={product.image}
+                  alt="Producto"
+                  className="w-full h-full object-cover object-center"
+                  loading="lazy"
+                />
+              </div>
+            </div>
+          </div>
+
+     
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 mt-12">
+            {/* Especificaciones */}
+            <div className="flex flex-col">
+              <h2 className="text-2xl font-bold text-gray-900 dark:text-white mb-4">
+                Specifications
+              </h2>
+              <div className="divide-y divide-subtle-light/60 dark:divide-border-black border-t border-b border-subtle-light/60 dark:border-border-dark  p-6">
+                <div className="py-4 grid grid-cols-3 gap-4">
+                  <p className="text-sm font-medium text-gray-500 dark:text-gray-400">
+                    Connectivity
+                  </p>
+                  <p className="text-sm col-span-2 text-gray-800 dark:text-gray-200">
+                    Bluetooth 5.0
+                  </p>
+                </div>
+                <div className="py-4 grid grid-cols-3 gap-4">
+                  <p className="text-sm font-medium text-gray-500 dark:text-gray-400">
+                    Battery Life
+                  </p>
+                  <p className="text-sm col-span-2 text-gray-800 dark:text-gray-200">
+                    Up to 30 hours
+                  </p>
+                </div>
+                <div className="py-4 grid grid-cols-3 gap-4">
+                  <p className="text-sm font-medium text-gray-500 dark:text-gray-400">
+                    Noise Cancellation
+                  </p>
+                  <p className="text-sm col-span-2 text-gray-800 dark:text-gray-200">
+                    Active Noise Cancellation
+                  </p>
+                </div>
+                <div className="py-4 grid grid-cols-3 gap-4">
+                  <p className="text-sm font-medium text-gray-500 dark:text-gray-400">
+                    Weight
+                  </p>
+                  <p className="text-sm col-span-2 text-gray-800 dark:text-gray-200">
+                    250g
+                  </p>
+                </div>
+                <div className="py-4 grid grid-cols-3 gap-4">
+                  <p className="text-sm font-medium text-gray-500 dark:text-gray-400">
+                    Color
+                  </p>
+                  <p className="text-sm col-span-2 text-gray-800 dark:text-gray-200">
+                    Midnight Black
+                  </p>
                 </div>
               </div>
             </div>
 
-            {/*  descripción y review */}
-            <div className="flex flex-col order-1 lg:order-2">
-              <div className="bg-gray-100 dark:bg-gray-800/50 rounded-xl p-6">
-                <h1 className="text-3xl font-bold text-gray-900 dark:text-white mb-2">
-                  Wireless Over-Ear Headphones
-                </h1>
-                <p className="text-gray-600 dark:text-gray-300 mb-4">
-                  Experience immersive sound with these wireless over-ear
-                  headphones, designed for comfort and exceptional audio
-                  quality. Perfect for music lovers and professionals alike.
-                </p>
-                <div className="flex items-baseline gap-2 mb-6">
-                  <span
-                    className="text-4xl font-black text-gray-900 dark:text-white font-display"
-                    content="299"
-                    itemProp="price"
-                  >
-                    $299
-                  </span>
-                </div>
-                <button className="w-full flex items-center justify-center rounded-lg h-12 px-6 bg-primary text-white text-base font-bold hover:bg-primary/90 transition-all">
-                  Add to Cart
-                </button>
-              </div>
-
-              <div className="mt-8 bg-background-light dark:bg-gray-800/50 border border-gray-200 dark:border-gray-800 rounded-xl p-6 shadow-sm">
+            {/* Review */}
+            <div className="flex flex-col">
+              <div className="bg-background-light dark:bg-gray-800/50 border border-gray-200 dark:border-gray-800 rounded-xl p-6 shadow-sm">
                 <h2 className="text-2xl font-bold text-gray-900 dark:text-white mb-4">
                   Expert Review
                 </h2>
@@ -145,7 +142,7 @@ const ProductPage = () => {
                           Audio Specialist
                         </p>
                       </div>
-                      <div className=" text-subtle-light dark:text-subtle-dark text-lg font-bold">
+                      <div className="text-subtle-light dark:text-subtle-dark text-lg font-bold">
                         8/10
                       </div>
                     </div>
