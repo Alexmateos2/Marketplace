87352da4795ab1c60421307185ef2817a2fb53cf
Alejandro <practicas2@sicosl.onmicrosoft.com>
2025-10-29 08:52:18 +0100

    Logo provisional implementado

--- src/shared/navbar/navbar.jsx
+++ src/shared/navbar/navbar.jsx
@@ -19,25 +19,22 @@ const Navbar = () => {
     <>
       <header className="flex items-center bg-background-light dark:bg-background-dark/80 backdrop-blur-sm sticky top-0 z-50 border-b border-primary/20 dark:border-primary/10">
         {!isLoginPage ? (
-          <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-2">
+          <div className="container mx-auto px-4  py-2">
             <div className="flex items-center justify-between h-16 w-full">
               <div className="flex items-center gap-2 lg:gap-8">
                 <NavLink
                   to="/"
-                  className="flex items-center gap-3 text-xl font-bold text-content-light dark:text-content-dark"
+                  aria-label="Inicio Tekia"
+                  className="flex  items-center gap-2 text-xl font-bold text-content-light dark:text-content-dark hover:opacity-80 transition-opacity"
                 >
-                  <svg
-                    className="h-6 w-6 text-primary"
-                    fill="none"
-                    viewBox="0 0 48 48"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M36.7273 44C33.9891 44 31.6043 39.8386 30.3636 33.69C29.123 39.8386 26.7382 44 24 44C21.2618 44 18.877 39.8386 17.6364 33.69C16.3957 39.8386 14.0109 44 11.2727 44C7.25611 44 4 35.0457 4 24C4 12.9543 7.25611 4 11.2727 4C14.0109 4 16.3957 8.16144 17.6364 14.31C18.877 8.16144 21.2618 4 24 4C26.7382 4 29.123 8.16144 30.3636 14.31C31.6043 8.16144 33.9891 4 36.7273 4C40.7439 4 44 12.9543 44 24C44 35.0457 40.7439 44 36.7273 44Z"
-                      fill="currentColor"
-                    />
-                  </svg>
-                  <span>Tekia</span>
+                  <img
+                    src="/logo2.svg"
+                    alt="Tekia logo"
+                    className="w-8 xxs:w-6 h-auto object-contain"
+                  />
+                  <span className="hidden xxs:block font-display font-semibold tracking-tight">
+                    Tekia
+                  </span>
                 </NavLink>
 
                 {/* Nav principal */}
@@ -108,8 +105,7 @@ const Navbar = () => {
 
                 <DarkMode />
                 <ProfileNavbar />
-              
-               
+                <NavLink to="/add">Add product</NavLink>
               </div>
             </div>
           </div>
@@ -180,10 +176,10 @@ const Navbar = () => {
 
           {!isProfilePage && !isLoginPage && !isAboutPage && (
             <div className="lg:hidden mb-5 px-4 flex items-center justify-start sm:justify-center pl-20 sm:pl-0">
-    <div className="w-full max-w-sm md:max-w-md">
-      <BarraBusqueda />
-    </div>
-  </div>
+              <div className="w-full max-w-sm md:max-w-md">
+                <BarraBusqueda />
+              </div>
+            </div>
           )}
         </>
       )}
